49cf7ddb4c48402b7c4e17fa9eec5e5c615c19db
ki-ask <site@kiask.xyz>
2023-05-12 04:13:37 +0600

    Updated App

--- src/footer.php
+++ src/footer.php
@@ -6,7 +6,7 @@
         <p>If you find this tool helpful and would like to support its development, you can do so in the following ways:</p>
           <p><a href="https://patreon.com/KiasK?utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link" target="_blank">Become a patron on Patreon</a></p>
           <p>Send cryptocurrency to the following address:</p>
-              <p>Ethereum, USDT, BUSD: 0x7c13a89b8E192e635ac24179B8DA6cC77101C651 on here</p>
+              <p>Ethereum, USDT, BUSD: 0x7c13a89b8E192e635ac24179B8DA6cC77101C651</p>
       </div>
     </div>
   </div>
