559b27886e1e6efef36f87da48cfba8a3cad6c21
ki-ask <site@kiask.xyz>
2023-05-12 04:30:22 +0600

    Updated App

--- src/header.php
+++ src/header.php
@@ -37,6 +37,7 @@ require_once(__DIR__ . '/../vendor/autoload.php');
 
   <!-- CSS stylesheet -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ki-ask/github-cdn@main/public/css/style.css">
 
   <!-- jQuery library -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
