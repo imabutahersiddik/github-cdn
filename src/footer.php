f202bbb7b82be66d9d03ea2c67195ce2f05847ad
ki-ask <site@kiask.xyz>
2023-05-12 04:21:14 +0600

    Updated App

--- src/footer.php
+++ src/footer.php
@@ -1,12 +1,12 @@
 <footer class="mt-5">
   <div class="container-fluid bg-light">
     <div class="row">
-      <div class="col-12 text-center">
-        <p>Developed by Abu Taher Siddik with ❤️ using Vercel and Bootstrap 5</p>
+      <div class="col-12 text-center">        
         <p>If you find this tool helpful and would like to support its development, you can do so in the following ways:</p>
           <p><a href="https://patreon.com/KiasK?utm_medium=clipboard_copy&utm_source=copyLink&utm_campaign=creatorshare_creator&utm_content=join_link" target="_blank">Become a patron on Patreon</a></p>
           <p>Send cryptocurrency to the following address:</p>
               <p>Ethereum, USDT, BUSD: 0x7c13a89b8E192e635ac24179B8DA6cC77101C651</p>
+         <p>Developed by Abu Taher Siddik with ❤️ using Vercel and Bootstrap 5</p>
       </div>
     </div>
   </div>
