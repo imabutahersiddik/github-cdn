e5f6331d554bcdc55682a38596f7eca0af7d5bd5
ki-ask <site@kiask.xyz>
2023-05-12 01:02:44 +0600

    Updated app

--- src/header.php
+++ src/header.php
@@ -4,7 +4,7 @@
 <?php
 // Load environment variables
 require_once(__DIR__ . '/../vendor/autoload.php');
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 ?>
 
